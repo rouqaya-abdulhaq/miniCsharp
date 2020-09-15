ce67312bf3e466070a9a18580f8966cc1ecb7118
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-09-15 23:27:17 +0300

    registered DB context

--- pizzaApi/Startup.cs
+++ pizzaApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using pizzaApi.Models;
 
 namespace pizzaApi
 {
@@ -25,6 +27,9 @@ namespace pizzaApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PizzaContext>(opt =>
+                opt.UseInMemoryDatabase("PizzaOrder")
+            );
             services.AddControllers();
         }
 
